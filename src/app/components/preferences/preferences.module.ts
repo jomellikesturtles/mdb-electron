98482d6fba9e3dc7194e63d09a8ae2a4f6d062f7
jommelsaligumba <jommelsaligumba@ust-global.com>
2021-05-09 12:38:54 +0800

    input container component

--- src/app/components/preferences/preferences.module.ts
+++ src/app/components/preferences/preferences.module.ts
@@ -2,24 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PreferencesRoutingModule } from './preferences.routing.module';
 import { PreferencesComponent } from './preferences.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { InputContainerModule } from '@components/shared/input-container/input-container.module';
 
 @NgModule({
   declarations: [PreferencesComponent],
   imports: [
-    FormsModule,
     CommonModule,
+    InputContainerModule,
+    FormsModule,
+    ReactiveFormsModule,
     PreferencesRoutingModule,
     SharedModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
     MatDialogModule,
+    MatCheckboxModule
   ],
   entryComponents: [
     PreferencesComponent,
