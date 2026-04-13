7027a6571db5d87fb8b36aa9d81a53c64cbdb9bd
jomellikesturtles <jommelsaligumba@gmail.com>
2026-04-13 21:30:22 +0800

    refactor material imports; region factory

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -7,6 +7,13 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
   exports: [
@@ -17,7 +24,14 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    MatAutocompleteModule
   ]
 })
 export class MaterialModule { }
